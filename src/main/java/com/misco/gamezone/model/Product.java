8e616a5836fee521182cec843825e8e8b76a98c8
JUAN MIGUEL DEVILLEROS PIMIENTA <jdevilleros@unicesar.edu.co>
2026-09-15 22:50:42 -0500

    feat: develop Product abstract class

--- src/main/java/com/misco/gamezone/model/Product.java
+++ src/main/java/com/misco/gamezone/model/Product.java
@@ -8,6 +8,51 @@ package com.misco.gamezone.model;
  *
  * @author USUARIO
  */
-public class Product {
-    
+public abstract class Product {
+
+    private String id;
+    private String name;
+    private double price;
+    private int stock;
+
+    public Product(String id, String name, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public abstract String getProductType();
 }
