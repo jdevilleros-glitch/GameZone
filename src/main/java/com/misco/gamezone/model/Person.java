d11b6e2b5764dd340066e7b819efeb44fd6df27a
JUAN MIGUEL DEVILLEROS PIMIENTA <jdevilleros@unicesar.edu.co>
2026-09-20 11:34:40 -0500

    feat: develop Person class

--- src/main/java/com/misco/gamezone/model/Person.java
+++ src/main/java/com/misco/gamezone/model/Person.java
@@ -8,6 +8,40 @@ package com.misco.gamezone.model;
  *
  * @author USUARIO
  */
-public class Person {
-    
+public abstract class Person {
+        private String name;
+        private String id;
+        private String cellphone;
+
+    public Person(String name, String id, String cellphone) {
+        this.name = name;
+        this.id = id;
+        this.cellphone = cellphone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+        
+    public abstract String getRole();
 }
